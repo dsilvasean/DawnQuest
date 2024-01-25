bd8b1dc0c9b97ebb8e5be8882b145650d0bbe51b
loopback failed <legacy@mail.com>
2024-01-26 01:48:38 +0530

    Shaalaa Spider is partially completed

--- backend/collector/collector/spiders/Shaalaa.py
+++ backend/collector/collector/spiders/Shaalaa.py
@@ -1,10 +1,10 @@
 import scrapy
 from asgiref.sync import sync_to_async
-import json
+import json, re
 from urllib.parse import quote
 
 from core.models import Meta
-from ShaalaaMiner.models import Publication, Grade
+from ShaalaaMiner.models import Publication, Grade, Chapter, Subject
 from collector.items import ShaalaaPublication
 
 
@@ -26,6 +26,10 @@ class ShaalaaSpider(scrapy.Spider):
         meta_object = Meta.objects.filter(spider__spider_name=self.name, attribute_name="publications_scraped").first()
         return meta_object.attribute_value if meta_object else None
     
+    def get_collected_subjects_before(self, grade, publication):
+        meta_object = Meta.objects.filter(spider__spider_name=self.name, attribute_name=f"subjects_scraped_grade_{grade}", attribute_extra__icontains=publication).first()
+        return meta_object.attribute_value if meta_object else None
+    
     def get_products_units_url(self):
         meta_object = Meta.objects.filter(spider__spider_name=self.name, attribute_name="get_products_units").first()
         if meta_object:
@@ -38,17 +42,45 @@ class ShaalaaSpider(scrapy.Spider):
             return meta_object.attribute_extras.filter(attribute_extra_name="url_chapters").first().attribute_extra_value
         return None
 
-    def get_publications_db(self):
-        publications_ = Publication.objects.filter(available=True, to_scrape=True).all()
+    def get_publications_db(self, name=None):
+        if name :
+            publications_ = Publication.objects.filter(available=True, to_scrape=True, author__icontains=name).all()
+            return publications_.first().id
+        else:
+            publications_ = Publication.objects.filter(available=True, to_scrape=True).all()
         if publications_:
             return [pub_.hyperlink for pub_ in publications_]
         return None
 
-    def get_grades(self):
-        grades = Grade.objects.filter(to_scrape=True).all()
-        if grades:
-            return [g_.grade for g_ in grades]
-        return None
+    def get_subjects_db(self,grade, publication):
+        if grade:
+            subjects = Subject.objects.filter(to_scrape=True,grade__grade=grade, publication__author__icontains=publication)
+            return [{"url": s.url, "shaalaa_id": s.shaalaa_id} for s in subjects]
+            
+
+    def get_chapters_to_scrape(self,subject):
+        if subject:
+            chapters = Chapter.objects.filter(subject__shaalaa_id=subject, to_scrape=True).all()
+            if chapters:
+                return [chap for chap in chapters]
+            
+    def update_chapter_url(self, chapter, url):
+        if chapter and url:
+            chapter_ = Chapter.objects.filter(name__icontains=chapter,).update(url=url)
+            print("updated" ,chapter_ , chapter)
+        if chapter_:
+            return True
+        return False
+
+    def get_grades(self, grade=None):
+        if grade:
+            grades = Grade.objects.filter(to_scrape=True, grade=grade).all()
+            return grades.first().id
+        else:
+            grades = Grade.objects.filter(to_scrape=True).all()
+            if grades:
+                return [g_.grade for g_ in grades]
+            return None
 
     async def parse(self, response):
         collected_publication_before = await sync_to_async(self.get_collected_publication_before)()
@@ -85,7 +117,7 @@ class ShaalaaSpider(scrapy.Spider):
 
         for grade in available_grades:
             product_url = f"{products_units_url}?id={int(grade['id'])}"
-            yield scrapy.Request(product_url, callback=self.parse_grades_url)
+            yield scrapy.Request(product_url, callback=self.parse_grades_url, cb_kwargs={"grade": grade})
 
         
         # if grades_ and available_grades:
@@ -101,12 +133,12 @@ class ShaalaaSpider(scrapy.Spider):
                 #     yield scrapy.Request(subject_tb_url, callback=self.parse_subject)
         
 
-    def parse_grades_url(self,response):
+    def parse_grades_url(self,response, grade):
         json_response = json.loads(response.text).get('content').get('data')[0]
         url_ = f"{self.root_url}{json_response.get('url2')}"
-        yield scrapy.Request(url_, callback=self.parse_grades_new, cb_kwargs={"course_id": json_response.get("unit_id")} ) 
+        yield scrapy.Request(url_, callback=self.parse_grades_new, cb_kwargs={"course_id": json_response.get("unit_id"), "grade": grade} ) 
     
-    async def parse_grades_new(self,response, course_id):
+    async def parse_grades_new(self,response, course_id,grade):
         subjects = []
        
         subjects_ = response.xpath("//input[contains(@class,'qp_filter')]")
@@ -125,23 +157,70 @@ class ShaalaaSpider(scrapy.Spider):
         }
         print(chapter_index_param)
         params_= quote(json.dumps(chapter_index_param))
-        yield scrapy.Request(f"{chapters_url}?type=textbookSolutions&content-type=course&params={params_}", callback=self.parse_chapter_names)
+        yield scrapy.Request(f"{chapters_url}?type=textbookSolutions&content-type=course&params={params_}", callback=self.parse_subjects, cb_kwargs={"grade":grade})
 
 
-    def parse_chapter_names(self, response):
-        pass
-    
-        
+    async def parse_subjects(self, response, grade):
+        json_response = json.loads(response.text).get("content").get("data")
+        pub_name = json_response.get("data")[0].get("author_names")[0]
+        subjects_and_lessons_scrape = await sync_to_async(self.get_collected_subjects_before)(grade=grade["grade"], publication=pub_name)
+        subjects_scrape = []
+
+        if not subjects_and_lessons_scrape:
+            publication_id = await sync_to_async(self.get_publications_db)(name=pub_name)
+            grade_id = await sync_to_async(self.get_grades)(grade=grade['grade'])
+            for _sub_ in json_response.get("data"):
+                export_data = {}
+                subject_info = {}
+                chapters = []
+                subject_info["name"] = _sub_.get("subject_name")
+                subject_info["sub_id"] = _sub_.get("id")
+                subject_info["descriptive_name"] = _sub_.get("name")
+                subject_info["url"] = f"{self.root_url}{_sub_.get('url2')}"
+                subject_info["publication_id"] = publication_id
+                subject_info["grade_id"] = grade_id
+                for chap in _sub_.get("products")[0].get("chapters"):
+                    chapters.append(chap)
+                export_data = {"subject" : subject_info, "chapters": chapters}
+                subjects_scrape.append(export_data)
+                yield {"item_data": export_data, "item_type":"subjects_and_chapters"}
+
+                # for subject in subjects_scrape:
+                #     yield scrapy.Request(subject["subject"]["url"], callback=self.parse_subject, cb_kwargs={"subject": subject["subject"]["sub_id"]})
+        else:
+            print("Publication lessons and chapters already collected check Meta.subjects_scraped_-grade- value")
+
+        subjects_to_scrape = await sync_to_async(self.get_subjects_db)(grade=grade["grade"],publication=pub_name)
+        for subject in subjects_to_scrape:
+            yield scrapy.Request(subject["url"], callback=self.parse_subject, cb_kwargs={"subject": subject["shaalaa_id"]})
+            
+            # for chap in json_response.get("data")
+            # yield {"item_data": _pub, "item_type":"publication"}
+            
 
 
-    def parse_subject(self, response):
-        available_chapters =  response.xpath(f"//div[contains(@class, 'block')]//a[contains(@href, 'chapter') and contains(normalize-space(), 'Chapter')]/@href").extract()[0:1]
-        for chapter in available_chapters:
-            chapter_url = f"{self.root_url}{chapter}"
-            yield scrapy.Request(chapter_url, callback=self.parse_chapter)
+    async def parse_subject(self, response, subject):
+        print(response)
+        available_chapters =await sync_to_async(self.get_chapters_to_scrape)(subject=subject)
+        available_chapters = [{ "chapter_name":f'{c.name.lower().replace(" ", "-")}', "id": c.id }
+                               for c in  available_chapters]
+        chapters_in_dom =  response.xpath(f"//div[contains(@class, 'block')]//a[contains(@href, 'chapter') and contains(normalize-space(), 'Chapter')]/@href").extract()
+        _chapters_ = [{"chapter_url": f"{self.root_url}{c}", "_chapter": avc}
+                      for avc in available_chapters 
+                      for c in chapters_in_dom 
+                      if re.search(re.escape(avc["chapter_name"]), c)]       
+        for chapter in _chapters_:
+            print(chapter)
+            # await sync_to_async(self.update_chapter_url)(chapter=chapter.split("/")[-1], url=chapter)
+            yield scrapy.Request(chapter["chapter_url"], callback=self.parse_chapter, cb_kwargs={"chapter_":chapter["_chapter"]["id"]  })
+
+        # available_chapters =  response.xpath(f"//div[contains(@class, 'block')]//a[contains(@href, 'chapter') and contains(normalize-space(), 'Chapter')]/@href").extract()[0:1]
+        # for chapter in available_chapters:
+        #     chapter_url = f"{self.root_url}{chapter}"
+        #     yield scrapy.Request(chapter_url, callback=self.parse_chapter)
 
 
-    def parse_chapter(self, response):
+    def parse_chapter(self, response, chapter_):
         questions_block = response.xpath(f"//div[contains(@class, 'qp_result_data')][@data-id]")
         for i, question_block in enumerate(questions_block):
             review_required= False
@@ -152,7 +231,10 @@ class ShaalaaSpider(scrapy.Spider):
                 question_type_text = questions_block[i-1].xpath(f".//div[contains(@class, 'html_text')]//strong/text()").extract()
             
             question_meta = question_block.xpath(f".//div[contains(@class, 'qp_result_data_data_meta')]/text()").extract()
-            question_ = question_block.xpath(f".//div[contains(@class, 'html_text')]//p[not(@class)]/text()").extract()
+            # question_ = question_block.xpath(f".//div[contains(@class, 'html_text')]//p[not(@class)]/text()").extract()
+            question_ = question_block.xpath(f".//div[contains(@class, 'html_wrap')]/*").extract()
+            
+            print(question_)
             if not question_:
                 question_ = question_block.xpath(f".//div[contains(@class, 'html_text')]/*[not(self::p[.//strong])]").extract()
                 review_required = True
@@ -165,6 +247,7 @@ class ShaalaaSpider(scrapy.Spider):
             # yield scrapy.Request()
 
             _question_ = {
+                'chapter_id': chapter_,
                 'review_required':review_required,
                 'question_type_text':question_type_text,
                 'question_meta':question_meta,
@@ -174,23 +257,26 @@ class ShaalaaSpider(scrapy.Spider):
             }
 
             if len(solution_href) > 4:
+                print(_question_)
                 yield scrapy.Request(solution_href, callback=self.parse_solution, priority=1, cb_kwargs={'_question_':_question_})
                 # print(_question_['question_'])
             # print(_question_)
     
     def parse_solution(self, response, _question_):
         _question_ = _question_
+        _solution_ = {}
         question_type = response.xpath("//div[contains(@class, 'qbq_q_type')]/text()").extract_first() or None
         solution_block  = response.xpath("//div[contains(@id, 'answer')]")
 
         if solution_block is not None:
-            _question_['solution_'] = "".join(solution_block.xpath(".//*/text()").extract())
+            # _solution_["solution_"] = "".join(solution_block.xpath(".//*/text()").extract())
+            _solution_["solution_"] = solution_block.xpath(".//descendant::*").get()
+            # _question_['solution_'] = "".join(solution_block.xpath(".//*/text()").extract())
         
-        _question_['solution_extra'] = None
-        _question_['question_type_from_solution'] = question_type
-        # print(solution_block)
-        
-        print(_question_)
+        _solution_['extra'] = None
+        _solution_['question_type_from_solution'] = question_type
+        _question_["_solution_"] = _solution_
+        yield {"item_data": _question_, "item_type":"question_and_solution"}
 
 
             
