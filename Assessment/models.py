5ee192973fbae44cd6d9143c956cd1972b5419bb
Sean D'silva <dsilvasean1@gmail.com>
2024-02-03 03:42:37 +0530

    added models for question paper

--- Assessment/models.py
+++ Assessment/models.py
@@ -1,23 +1,36 @@
-# from django.db import models
+from django.db import models
 
-# from core.models import Board, Grade, Subject
-# from ShaalaaMiner.models import Question as ShaalaaQuestions
+from core.models import TimeStampAbstractModel, MetaAbstarctModel
+from core.models import Board, Grade, Subject, Question, QuestionType
 
-# # Create your models here.
+class QuestionPaper(TimeStampAbstractModel, MetaAbstarctModel):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    grade = models.ForeignKey(Grade, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    max_marks = models.IntegerField()
+    questions = models.ManyToManyField(Question, through='QuestionPaperQuestionRelation')
 
-# class QuestionPaper(models.Model):
-#     max_marks = models.IntegerField()
-#     board = models.ForeignKey(Board, on_delete=models.CASCADE)
-#     grade = models.ForeignKey(Grade, on_delete=models.CASCADE)
-#     questions = models.ManyToManyField(ShaalaaQuestions, through='QuestionPaperQuestion')
-#     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
 
-# class QuestionPaperQuestion(models.Model):
-#     question_paper = models.ForeignKey(QuestionPaper, on_delete=models.CASCADE)
-#     questions = models.ForeignKey(ShaalaaQuestions, on_delete=models.CASCADE)
-#     index = models.IntegerField()
-#     marks = models.IntegerField()
-#     meta = models.CharField(max_length=255)
+class QuestionPaperQuestionRelation(TimeStampAbstractModel, MetaAbstarctModel):
+    question_paper = models.ForeignKey(QuestionPaper, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    order = models.IntegerField()
+    marks = models.IntegerField()
+
+
+class QuestionPaperFormat(TimeStampAbstractModel,):
+    grade = models.ForeignKey(Grade, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+class QuestionPaperFormatQuestion(TimeStampAbstractModel,):
+    question_head = models.CharField(max_length=255)
+    question_paper_format = models.ForeignKey(QuestionPaperFormat, on_delete=models.CASCADE)
+
+class QuestionPaperFormatQuestionSubQuestion(TimeStampAbstractModel):
+    question_paper_format_question = models.ForeignKey(QuestionPaperFormatQuestion, on_delete=models.CASCADE)
+    question_type = models.ManyToManyField(QuestionType)
+    max_marks = models.IntegerField()
+
 
 
 
